ea05900ba23d5b7f3185de3515163535003bf61a
QQ565253906 <565253906@qq.com>
2018-03-29 23:03:07 +0800

    step2
    parse current page only

--- spiders/douban_spider.py
+++ spiders/douban_spider.py
@@ -1,16 +1,22 @@
 #coding:utf-8
+from scrapy import Request
 from scrapy.spiders import Spider
-
-
 from scrapyspider_01.items import DoubanMovieItem
 
 
 class DoubanMovieTop250Spider(Spider):
     name = 'douban_movie_top250'
-    start_urls = ['https://movie.douban.com/top250']
-    
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
+    }
+
+    def start_requests(self):
+        url = 'https://movie.douban.com/top250'
+        yield Request(url, headers=self.headers)
+
     def parse(self, response):
-        item = DoubanMovieItem()	
+        item = DoubanMovieItem()
+        movies = response.xpath('//ol[@class="grid_view"]/li')
         for movie in movies:
             item['ranking'] = movie.xpath(
                 './/div[@class="pic"]/em/text()').extract()[0]
@@ -21,4 +27,4 @@ class DoubanMovieTop250Spider(Spider):
             ).extract()[0]
             item['score_num'] = movie.xpath(
                 './/div[@class="star"]/span/text()').re(ur'(\d+)人评价')[0]
-            yield item		
+            yield item	
